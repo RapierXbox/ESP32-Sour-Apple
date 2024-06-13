ce761918f3334f72f9f26459b09e8fb817340e71
FLOCK4H <161654571+FLOCK4H@users.noreply.github.com>
2024-06-13 17:50:51 +0200

    Update boot.py

--- scripts/sour-apple-circuitpython/boot.py
+++ scripts/sour-apple-circuitpython/boot.py
@@ -1,3 +1,6 @@
-import supervisor
+# the disable_ble_workflow() was removed in CircuitPython 8.2.x release
+# uncomment the lines below if CircuitPython version <= 7.3
 
-supervisor.disable_ble_workflow()
+# import supervisor
+
+# supervisor.disable_ble_workflow()
